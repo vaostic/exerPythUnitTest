ad6e22afee9a2883f897c737cecfaad6a2d00e5d
vaostic <u1479673@umail.utah.edu>
2023-10-17 13:51:26 -0600

    cmt1

---
+++ calc.py
@@ -0,0 +1,19 @@
+def add (x, y):
+    return x + y
+
+
+
+def subtract(x, y):
+    return x - y
+
+
+
+def multiply (x,y):
+    return x * y
+
+
+
+def divide (x,y):
+    if y == 0:
+        raise ValueError
+    return x / y
